4016c44b3b7762abf8a5ea16123c64f10a124859
mariamboules9876-blip <mariamboules9876@gmail.com>
2026-05-01 06:12:04 -0700

    Q1

--- EF-Session4/Program.cs
+++ EF-Session4/Program.cs
@@ -1,10 +1,77 @@
-﻿namespace EF_Session4
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_Session4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+//            using System;
+//            using System.Collections.Generic;
+//            using System.ComponentModel.DataAnnotations;
+
+//namespace BankManagementSystem.Models
+//    {
+//        public class Branch
+//        {
+//            [Key]
+//            public int BranchCode { get; set; }
+//            public string Name { get; set; }
+//            public string Address { get; set; }
+//            public string PhoneNumber { get; set; }
+
+//            public Manager BranchManager { get; set; }
+//        }
+
+//        public class Manager
+//        {
+//            public int Id { get; set; }
+//            public string FullName { get; set; }
+//            public string Email { get; set; }
+//            public string Phone { get; set; }
+//            public DateTime HireDate { get; set; }
+//        }
+
+//        public class Customer
+//        {
+//            public int Id { get; set; }
+//            public string Name { get; set; }
+//            public string NationalID { get; set; }
+//            public string Email { get; set; }
+//            public string Address { get; set; }
+
+//            public int BranchId { get; set; }
+//            public List<Account> Accounts { get; set; }
+//        }
+
+//        public class Account
+//        {
+//            [Key]
+//            public int AccountNumber { get; set; }
+//            public string Type { get; set; }
+//            public decimal Balance { get; set; }
+//            public DateTime CreatedDate { get; set; }
+
+//            public List<Customer> Owners { get; set; }
+//            public List<Transaction> Transactions { get; set; }
+//        }
+
+//        public class Transaction
+//        {
+//            public int TransactionId { get; set; }
+//            public decimal Amount { get; set; }
+//            public string Type { get; set; }
+//            public string Note { get; set; }
+//            public DateTime Date { get; set; }
+//        }
+//    }
+
+
+
+    Console.WriteLine("Hello, World!");
         }
     }
 }
